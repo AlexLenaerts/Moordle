1a298839c438de59112a30b2d1c5dcb0ce456a5e
Alexandre Lenaerts <17028176+AlexLenaerts@users.noreply.github.com>
2022-03-01 12:03:25 +0100

    Update site.js

--- Moordle/js/site.js
+++ Moordle/js/site.js
@@ -15,35 +15,11 @@ const getWordle = () => {
 }
 getWordle()
 
+
 const keys = [
-    'Q',
-    'W',
-    'E',
-    'R',
-    'T',
-    'Y',
-    'U',
-    'I',
-    'O',
-    'P',
-    'A',
-    'S',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
-    'ENTER',
-    'Z',
-    'X',
-    'C',
-    'V',
-    'B',
-    'N',
-    'M',
-    '«',
+    'A','Z','E','R','T','Y','U','I','O','P',
+    'Q','S','D','F','G','H','J','K','L','M',
+    'W', 'X', 'C', 'V', 'B', 'N', '«', 'ENTER'
 ]
 const guessRows = [
     ['', '', '', '', ''],
